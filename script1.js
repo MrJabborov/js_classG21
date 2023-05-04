27681e29f3f8a2e0e3aaac952a4401c7e5e00175
Zukhriddin Jabborov <j.zuhriddin.95@gmail.com>
2023-05-04 22:36:25 +0900

    type_conversion, comparision and logical operators

---
+++ script1.js
@@ -0,0 +1,18 @@
+// HOMEWORK 1-1
+var number = 4
+number % 2 === 0 && console.log('Juft son');
+number % 2 !== 0 && console.log('Toq son');
+
+// HOMEWORK 1-2
+var ball = 43
+ball <= 100 && ball >=86 && console.log('A');
+ball <= 85 && ball >=61 && console.log('B');
+ball <= 60 && ball >=50 && console.log('C');
+ball < 50 && console.log('FAILED :(');
+
+// HOMEWORK 1-3
+var meva = 'pomidor'
+meva === 'olma' && console.log('10.000');
+meva === 'nok' && console.log('12.000');
+meva === 'travuz' && console.log('20.000');
+meva !== 'olma' && meva !== 'nok' && meva !== 'tarvuz' && console.log('Bunday meva yoq'); ;
