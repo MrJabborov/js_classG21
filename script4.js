fd4a4114ba6932c64ae18acbb99441a1c7a28f8a
Zukhriddin Jabborov <j.zuhriddin.95@gmail.com>
2023-05-09 23:39:20 +0900

    while and Do-while Loops

---
+++ script4.js
@@ -0,0 +1,25 @@
+// HOMEWORK 4-1
+                       //with WHILE LOOP
+let i = 1;
+while (i <= 5){
+    if(i % 2 === 0){
+        console.log(i,'Odd Number');
+    }else {
+        console.log(i,'Even Number');
+    }
+    i++
+}
+
+                        //with DO/WHILE LOOP
+
+let j = 1;
+do{
+    if(j % 2 === 0){
+        console.log(j, 'Odd Number');
+    }else{
+        console.log(j, 'Even Number');
+    }
+    j++
+}while(j <= 5);
+
+
