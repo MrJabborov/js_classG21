a5e49eb79b224c1f540458de5b88d4aeba3310fd
Zukhriddin Jabborov <j.zuhriddin.95@gmail.com>
2023-05-17 20:39:11 +0900

    Math.methods and Number data type

---
+++ script5.js
@@ -0,0 +1,58 @@
+// HOMEWORK 5-1
+function MathPow(son, daraja) {
+    return son ** daraja
+}
+console.log(MathPow(5,2));
+
+
+// HOMEWORK 5-2
+function MathSqrt(son, daraja) {
+    return son ** (1/2)
+}
+console.log(MathSqrt(64));
+
+
+// // HOMEWORK 5-3
+function MathCbrt(son) {
+    return Math.sqrt()
+}
+console.log(MathCbrt(64));
+
+
+
+// HOMEWORK 5-4
+function MathAbs(son) {
+    if(son > 0 || son == 0)
+    return a
+
+    else {
+       return a-a-a
+    }
+}
+console.log(MathAbs(-1995));
+
+
+// HOMEWORK 5-5
+function MathRound(a) {
+     if(a > 0,5 || a == a)
+     return a
+
+     else {
+        return a + 1
+     }
+}
+console.log(MathRound(5.4));
+
+
+// HOMEWORK 5-6
+function MathFloor (son) {
+        return son 
+    }
+    console.log(parseInt(MathFloor (3,8)));
+
+
+// HOMEWORK 5-7
+function MathRound (son) {
+    return son 
+}
+console.log(MathRound (20.4).toFixed() );
