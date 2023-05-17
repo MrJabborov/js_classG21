2a4052865870ec5bb476b211d42d3931b882bb97
Zukhriddin Jabborov <j.zuhriddin.95@gmail.com>
2023-05-17 20:42:08 +0900

    String data type and its build-in methods

---
+++ script6.js
@@ -0,0 +1,47 @@
+// HOMEWORK 6-1
+let name = 'FoziL'
+console.log(name.endsWith('l') || name.endsWith('L'));
+
+// HOMEWORK 6-2
+var str1 = 'Web'
+var str2 = 'Brain'
+
+str1 = 'Brain'
+str2 = 'Web'
+
+console.log(str2);
+
+
+// HOMEWORK 6-3
+let year = 2021;
+
+if(year % 4 === 0){
+    console.log('Kabisa yili');
+}else{
+    console.log('Kabisa yili emas');
+}
+
+
+// HOMEWORK 6-4
+
+
+
+// HOMEWORK 6-5
+let string = '#fozil#web#brain#'
+console.log(string.replace('#fozil#web#brain#' , '(fozil)web(brain)' ));
+
+// HOMEWORK 6-6
+let str = 'WeBbraIN'
+let i = 0
+let emptyStr = '';
+
+while(i < str.length){
+    if (str[i] === str[i].toUpperCase())
+    emptyStr += str[i]
+    i++
+}
+console.log(emptyStr);
+
+// HOMEWORK 6-7
+let string1 = 'Webbrain  '
+console.log(string1.replace('Web', 'IT').repeat (10))
